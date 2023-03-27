ae4772c528cddb31756552e17740420aaa30156d
tomasitok <tomasaristizabalgomez@gmail.com>
2023-03-27 16:27:04 -0500

    punto 6

---
+++ 6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf ("Size of a char: %zd byte(s)\n",sizeof(char));
+    printf ("Size of an int: %zd byte(s)\n",sizeof(int));
+    printf ("Size of a long int: %zd byte(s)\n",sizeof(long int));
+    printf ("Size of a long long int: %zd byte(s)\n",sizeof(long long int));
+    printf ("Size of a float: %zd byte(s)\n",sizeof(float));
+
+
+
+}
